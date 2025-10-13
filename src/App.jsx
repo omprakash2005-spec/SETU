6da1470ba03f7fe2d61126e77114503ebd1d7af2
ChillGuy007 <coder.pritam.2005@gmail.com>
2025-10-14 00:48:54 +0530

    Redeem page added

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import Events from "./pages/Events";
 import AddEvents from "./pages/AddEvents";
 import Post from "./pages/Post";
 import MentorProfile from "./pages/MentorProfile";
+import Donations from "./pages/Donations";
+import Redeem from "./pages/Redeem";
 // import Feed from "./pages/Feed";
 
 function App() {
@@ -26,6 +28,8 @@ function App() {
         <Route path="/addEvents" element={<AddEvents />} />
         <Route path="/post" element={<Post />} />
         <Route path="/mentor/:name" element={<MentorProfile />} />
+        <Route path="/Donations" element={<Donations />} />
+        <Route path="/Redeem" element={<Redeem />} />
         {/* <Route path="/feed" element={<Feed />} /> */}
       </Routes>
     </div>
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,10 +3,16 @@ import logo from "./logo.svg"
 import homeBG from "./homeBG.webp"
 import profile from "./profile.jpeg"
 import donation from "./Donation.jpg"
+import discount from "./discount.jpg"
+import event from "./party.jpg"
+import event1 from "./event.jpg"
 export const assets = {
     landingBG,
     logo,
     homeBG,
     profile,
     donation,
+    discount,
+    event,
+    event1,
 }
